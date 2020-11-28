a40da4c3789ecd937d2770120fb45f2b2f18a0d7
Sonu Simon <sonuisplaying@gmail.com>
2020-11-29 02:22:07 +0530

    firebase better

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -3,41 +3,11 @@ import 'package:flutter/material.dart';
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
 
-class App extends StatelessWidget {
-  // Create the initialization Future outside of `build`:
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+String firebaseState = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(
-              child: Text('Something went wrong!'),
-            ),
-          );
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Scaffold(
-            body: Center(
-              child: Text('Yay! Success'),
-            ),
-          );
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return Scaffold(
-          body: Center(
-            child: Text('Loading happening now!'),
-          ),
-        );
-      },
-    );
-  }
+initializeFirebaseApp() {
+  Firebase.initializeApp().then((_) {
+    print('Firebase gets initialized');
+    firebaseState = 'Firebase initialized successfully';
+  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:demand_supply/firebase.dart';
+import 'package:demand_supply/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initializeFirebaseApp();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: App(),
+      home: HomePage(),
     );
   }
 }
