c0f3332614dfa4b95d198deff0f5e9d0cc7e2c1a
sonu-simon <sonuisplaying@gmail.com>
2020-12-20 20:38:21 +0530

    searchpage fixed

--- lib/screens/search/searchPage.dart
+++ lib/screens/search/searchPage.dart
@@ -31,84 +31,77 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-      ),
-      body: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            centerTitle: true,
-            actions: [
-              IconButton(
-                  icon: Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                    size: 40,
-                  ),
-                  onPressed: searchFn)
-            ],
-            title: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: TextFormField(
-                style: TextStyle(fontSize: 20, color: Colors.black87),
-                autofocus: true,
-                maxLines: 1,
-                decoration: InputDecoration(
-                    focusColor: Colors.black,
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue)),
-                    hintText: "Search",
-                    hintStyle: TextStyle(fontSize: 20)),
-                initialValue: "",
-                textInputAction: TextInputAction.search,
-                onFieldSubmitted: (value) {
-                  searchTerm = value;
-                  searchFn();
-                  print('search term $value');
-                },
-                onChanged: ((String newValue) {
-                  // uName = newValue;
-                }),
-              ),
-            ),
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 12.0),
+          child: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () => Navigator.pop(context)),
+        ),
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
+          child: TextFormField(
+            style: TextStyle(fontSize: 20, color: Colors.black87),
+            autofocus: true,
+            maxLines: 1,
+            decoration: InputDecoration(
+                suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.blue,
+                    ),
+                    onPressed: searchFn),
+                focusColor: Colors.black,
+                hintText: "Search here",
+                hintStyle: TextStyle(fontSize: 20)),
+            initialValue: "",
+            textInputAction: TextInputAction.search,
+            onFieldSubmitted: (value) {
+              searchTerm = value;
+              searchFn();
+              print('search term $value');
+            },
           ),
-          body: advancedSearchList.isEmpty
-              ? Center(
-                  child: Text('No results found'),
-                )
-              : ListView.separated(
-                  itemBuilder: (context, index) => Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Material(
-                          elevation: 2,
-                          child: ListTile(
-                            onTap: () {
-                              showLoading(context, true);
-                              postByPostPath(advancedSearchList[index].postPath)
-                                  .then((post) {
-                                showLoading(context, false);
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            ProductPage(post)));
-                              });
-                            },
-                            title: Text(
-                              advancedSearchList[index].title,
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ),
+        ),
+      ),
+      body: advancedSearchList.isEmpty
+          ? Center(
+              child: Text('No results found'),
+            )
+          : ListView.separated(
+              itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Material(
+                      elevation: 3,
+                      color: Colors.grey[100],
+                      child: ListTile(
+                        onTap: () {
+                          showLoading(context, true);
+                          postByPostPath(advancedSearchList[index].postPath)
+                              .then((post) {
+                            showLoading(context, false);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ProductPage(post)));
+                          });
+                        },
+                        title: Text(
+                          advancedSearchList[index].title,
+                          style: TextStyle(fontSize: 20),
                         ),
                       ),
-                  separatorBuilder: (context, index) {
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Divider(
-                        color: Colors.grey,
-                      ),
-                    );
-                  },
-                  itemCount: advancedSearchList.length)),
+                    ),
+                  ),
+              separatorBuilder: (context, index) {
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Divider(
+                    color: Colors.grey,
+                  ),
+                );
+              },
+              itemCount: advancedSearchList.length),
     );
   }
 }
